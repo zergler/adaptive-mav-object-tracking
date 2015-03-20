49f85b111261d11a151fb0a8f3e3ac1fe4cb35e3
Igor Janjic <i.janjic@live.com>
2015-03-19 21:40:28 -0400

    Added opencv support for parrot png streams

---
+++ src/parrot/drone-net.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo systemctl stop netctl-auto@wlp16s0.service
+sudo netctl start Parrot
---
+++ src/parrot/repl.js
@@ -0,0 +1,4 @@
+var arDrone = require('ar-drone');
+var client  = arDrone.createClient();
+client.createRepl();
+
